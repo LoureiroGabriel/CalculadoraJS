42c37cde04f2f792bb94a4202c733030fbd29076
Gabriel Loureiro <loureirogabrieloficial@gmail.com>
2023-10-20 22:50:31 -0300

    Novas funcionalidades

--- calculadora.js
+++ calculadora.js
@@ -1,9 +1,21 @@
 const main = document.querySelector('main')
 const root = document.querySelector(':root')
 const input = document.getElementById('input')
-const resultInput = document.getElementById('result')
+let resultInput = document.getElementById('result')
 
 const allowedKeys = ["(", ")", "/", "*", "-", "+", "9", "8", "7", "6", "5", "4", "3", "2", "1", "0", ".", "%", " "]
+
+document.querySelectorAll('.charKey').forEach(function (charKeyBtn){
+ charKeyBtn.addEventListener('click', ()=>{
+    const value = charKeyBtn.dataset.value
+    input.value += value
+ })
+})
+
+document.getElementById('clear').addEventListener('click', ()=>{
+    input.value = ''
+    input.focus()
+})
 
 input.addEventListener('keydown', (ev) => {
     ev.preventDefault()
@@ -13,5 +25,15 @@ input.addEventListener('keydown', (ev) => {
     }
     if (ev.key === 'Backspace') {
         input.value = input.value.slice(0, -1)
+    }if (ev.key === 'Enter'){
+        calculate()
     }
 })
+
+document.getElementById('equal').addEventListener('click',calculate)
+
+function calculate(){
+    const result = eval(input.value)
+    resultInput.value = result
+
+}
